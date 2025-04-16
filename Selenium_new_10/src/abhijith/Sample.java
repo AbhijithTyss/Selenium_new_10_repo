61b13c77e5ddb92461dd196f158713c3461eb059
AbhijithTyss <abhijith.styss@gmail.com>
2025-04-16 11:46:00 +0530

    Added sample class

---
+++ Selenium_new_10/src/abhijith/Sample.java
@@ -0,0 +1,11 @@
+package abhijith;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	public void test() {
+		Reporter.log("Sample class added",true);
+	}
+}
